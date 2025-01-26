11e5473c8edcf4d6e3a5cce771e507e8828d43ec
Winnee0305 <winnee.chew@gmail.com>
2025-01-26 14:43:35 +0800

    Followed sample notebook

--- main.py
+++ main.py
@@ -50,17 +50,70 @@ from pystac.extensions.eo import EOExtension as eo
 import os
 from tqdm import tqdm
 
-# Global vairable for file names
-trainingDataFileName = "Training_data_uhi_index.csv"
-submissionFileName = "Submission_template.csv"
+# Import common GIS tools
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+import rioxarray as rio
+import rasterio
+from matplotlib.cm import RdYlGn,jet,RdBu
 
-ground_df = pd.read_csv(trainingDataFileName)
+# Import Planetary Computer tools
+import stackstac
+import pystac_client
+import planetary_computer 
+from odc.stac import stac_load
+
+# Global vairable for file names
+training_data_filename = "Training_data_uhi_index.csv"
+submission_filename = "Submission_template.csv"
+
+ground_df = pd.read_csv(training_data_filename)
 ground_df.head()
 print(ground_df.shape)
 
-submission = pd.read_csv(submissionFileName)
+submission = pd.read_csv(submission_filename)
 submission.head()
 print(submission.shape)
 
 
+def createBoundsForSinglePoint (latitude, longitude, margin):
+    point = (latitude, longitude)
+    lower_left = (point[0] - margin, point[1] - margin)  
+    upper_right = (point[0] + margin, point[1] + margin) 
+    bounds = (lower_left[1], lower_left[0], upper_right[1], upper_right[0])
+    return bounds
 
+# Define the time window
+margin = 0.001
+time_window = "2021-06-01/2021-09-01"
+
+stac = pystac_client.Client.open("https://planetarycomputer.microsoft.com/api/stac/v1")
+
+
+search = stac.search(
+    bbox=createBoundsForSinglePoint(40.81310667,-73.90916667,margin),
+    datetime=time_window,
+    collections=["sentinel-2-l2a"],
+    query={"eo:cloud_cover": {"lt": 30}},
+)
+
+items = list(search.get_items())
+print('This is the number of scenes that touch our region:',len(items))
+
+signed_items = [planetary_computer.sign(item).to_dict() for item in items]
+
+resolution = 10  # meters per pixel 
+scale = resolution / 111320.0 # degrees per pixel for crs=4326 
+
+
+data = stac_load(
+    items,
+    bands=["B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B11", "B12"],
+    crs="EPSG:4326", # Latitude-Longitude
+    resolution=scale, # Degrees
+    chunks={"x": 2048, "y": 2048},
+    dtype="uint16",
+    patch_url=planetary_computer.sign,
+    bbox=createBoundsForSinglePoint(40.81310667,-73.90916667,margin),
+)
