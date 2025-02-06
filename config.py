d06d7993fe1ee7ff2356009cd5f9dc35a3cbf714
Winnee0305 <winnee.chew@gmail.com>
2025-02-06 17:31:49 +0800

    Restructured code in few notebooks, added config py script, generalized code block

---
+++ config.py
@@ -0,0 +1,54 @@
+### This file is used for global variable declaration ###
+
+
+import pandas as pd
+
+training_data = "training_data_uhi_index.csv"
+
+# File to store dataset
+
+sentinel2_tiff = "sentinel2.tiff"
+
+landsat_1_tiff = "landsat_1.tiff"
+
+landsat_2_tiff = "landsat_2.tiff"
+
+submission_template = "submission_template.csv"
+
+## Variables to extract from dataset
+
+# Sentinel 2 Bands
+sentinel2_bands = ['B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B09', 'B11', 'B12']
+
+# Landsat 8 Bands
+landsat_1_bands = ["blue","green","red","nir08","swir16","swir22","coastal"]
+
+landsat_2_bands = ["lwir11"]
+
+## Read the training data and submission template to declare the global variables
+
+training_df = pd.read_csv(training_data, header=0, sep=',')
+training_df.head()
+# print("Size of the training data: " , training_df.shape)
+
+submission = pd.read_csv(submission_template)
+submission.head()
+# print("Size of the submission data: " , submission.shape)
+
+# Extract the minimum and maximum latitude and longitude from the dataset
+min_lat = training_df['Latitude'].min()
+max_lat = training_df['Latitude'].max()
+min_lon = training_df['Longitude'].min()
+max_lon = training_df['Longitude'].max()
+
+lower_left = (min_lat, min_lon)
+upper_right = (max_lat, max_lon)
+
+# Define the bounding box using the min/max values
+bounds = (lower_left[1], lower_left[0], upper_right[1], upper_right[0])
+
+# Print the defined bounds
+# print(f"Area of Interest Bounding Box: {bounds}")
+
+# Define the time window
+time_window = "2021-06-01/2021-09-01"
